872e5e92144825fd5db09ae958de096684f9945f
ararity <epicbwo@gmail.com>
2023-03-05 14:02:05 -0800

    Created main.py with menu for encode/decode selection

---
+++ main.py
@@ -0,0 +1,26 @@
+########################################
+#              COP3502C                #
+#              Lab 6                   #
+#              Encoder/Decoder         #
+#              Blake Owen, Braden Azis #
+########################################
+
+
+if __name__ == __main__:
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        select = int(input("Please enter an option: "))
+
+        if select == 1:
+            pass
+        elif select == 2:
+            pass
+        elif select == 3:
+            quit()
+        else:
+            print("Error: Invalid selection")
